1ede53d965f86dac09179bf1d2fba0dfe6ad635f
PaulinoMoskwa <pau.moskwa@gmail.com>
2022-05-18 20:51:32 +0200

    Update

--- main.py
+++ main.py
@@ -2,16 +2,6 @@
 # Libraries
 # -----------------------------------------------------------------------------------------------------------------------
 import streamlit as st
-from sklearn import datasets
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC # support vector classifier
-from sklearn.ensemble import RandomForestClassifier
-import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
-
 
 # -----------------------------------------------------------------------------------------------------------------------
 # Title
